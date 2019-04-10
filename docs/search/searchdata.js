a1f02149e59a00fc35d8cb9a712ccba7ad9d4701
Paul Breugnot <paul.breugnot@etu.emse.fr>
2019-04-10 19:38:41 +0200

    Move Doc

---
+++ docs/search/searchdata.js
@@ -0,0 +1,30 @@
+var indexSectionsWithContent =
+{
+  0: "bceghlrsw~",
+  1: "rsw",
+  2: "l",
+  3: "chrsw~",
+  4: "bgrw",
+  5: "e"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Pages"
+};
+
